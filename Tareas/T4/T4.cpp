e402d20777bec0961e8bd64c81cec544c18fab7c
Deivagt <58482545+Deivagt@users.noreply.github.com>
2020-02-10 23:51:53 -0600

    Tarea 4

---
+++ Tareas/T4/T4.cpp
@@ -0,0 +1,163 @@
+
+#include <iostream>
+using namespace std;
+
+
+class nodo 
+{
+private:
+
+    char letra;
+    int id;
+    nodo *anterior;
+    nodo *siguiente;
+
+    friend class listaD;
+};
+
+class listaD 
+{
+private:
+    nodo *primero;
+    nodo *ultimo;
+    
+
+public:
+    listaD();
+    void insertarPrimero(char c);
+    void eliminarUltimo();
+    void buscar(string s);
+    void imprimir();
+
+
+
+
+};
+
+int main()
+{
+
+    listaD* hola = new listaD;
+    
+    string s = "hola";
+    hola->insertarPrimero(' ');
+   
+    hola->insertarPrimero('h');
+    hola->insertarPrimero('o');
+    hola->insertarPrimero('l');
+    hola->insertarPrimero('e');
+   
+
+
+
+    hola->buscar(s);
+  
+
+}
+
+listaD::listaD(){
+    primero = new nodo();
+    ultimo = new nodo();
+    primero->siguiente = ultimo;
+    primero->anterior = NULL;
+
+    ultimo->anterior = primero;
+    ultimo->siguiente = NULL;
+
+
+
+}
+
+void listaD::insertarPrimero(char c) 
+{
+    nodo* aux = new nodo();
+    aux->letra = c;
+    aux->anterior = primero;
+    aux->siguiente = primero->siguiente;
+
+    primero->siguiente->anterior = aux;
+    primero->siguiente = aux;
+
+    }
+void listaD::eliminarUltimo() {
+    nodo* aux = new nodo();
+
+    if (!(primero->siguiente == ultimo && ultimo->anterior == primero)) {
+        aux = ultimo->anterior;
+
+        aux->anterior->siguiente = ultimo;
+        ultimo->anterior = aux->anterior;
+
+        aux->siguiente = NULL;
+        aux->anterior = NULL;
+     
+     
+    }
+    else {
+        cout << "La lista esta vacia"<< endl;
+    }
+   
+
+
+}
+
+void listaD::imprimir() {
+    nodo *aux = new nodo();
+    aux = ultimo->anterior;
+    if (aux->anterior == NULL) {
+        cout << "La lista esta vacia" << endl;
+    }
+    else {
+        
+        while(aux != primero) {
+            cout << aux->letra ;
+            aux = aux->anterior;
+        }
+    }
+}
+
+void listaD::buscar(string s) {
+    
+    char buffer[100];
+    int tB = 0;
+
+    strcpy_s(buffer, s.c_str());
+ 
+    
+    nodo* aux = new nodo();
+    nodo* temp = new nodo();
+    aux = ultimo->anterior;
+  
+    bool match = false;
+
+    if (aux->anterior == NULL) {
+        cout << "La lista esta vacia" << endl;
+    }
+    else {
+    
+        while (aux != primero ) {
+            if (aux->letra == buffer[tB]) {
+
+                if (tB == 0) {
+                    temp = aux;
+                }
+
+                match = true;
+
+                tB++;
+            }
+        
+          
+        
+            aux = aux->anterior;
+            
+        }
+        if (match == true) {
+            cout << "La primera letra es: " << temp->letra << endl;
+        }
+        else {
+            cout << "Palabra no encontrada" << endl;
+        }
+    }
+
+}
